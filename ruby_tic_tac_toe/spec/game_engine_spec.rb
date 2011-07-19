735a0031789f8e48877c16577479427dc31eb3e3
Dave Moore <dcmoore@dcmoore.(none)>
2011-07-19 17:21:49 -0500

    End of the day commit

--- ruby_tic_tac_toe/spec/game_engine_spec.rb
+++ ruby_tic_tac_toe/spec/game_engine_spec.rb
@@ -3,7 +3,8 @@ require 'game_engine'
 
 describe GameEngine do
   #TODO - How am I supposed to run a test on an object that requires user input?
-  #TRYTHIS - http://technicalpickles.com/posts/a-pattern-for-using-standard-in-and-out-in-your-ruby-code/
-  #TRYTHIS - io = mock('io', :chomps => 2)
-  #          System.should_receive(:gets).and_returns(io)
+  #TRYTHIS - http://stackoverflow.com/questions/6335282/testing-with-stdin-and-stdout-in-rspec
+  #TRYTHIS - https://gist.github.com/194554
+
+
 end
